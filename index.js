9328decccc9400222421ee4a884e215095478366
contehboy <133304258+contehboy@users.noreply.github.com>
2023-08-04 06:22:47 -0500

    add port

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// app.js
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("app is running");
